84267e86e485f68a1795fdb08bac533393de620f
chillbro20 <97622677+chillbro20@users.noreply.github.com>
2022-05-23 16:35:58 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
+from flask_sqlalchemy import SQLAlchemy
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    created_on = db.Column(db.DateTime, index=True, default=datetime.now)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)   
+
+def load_user(id):
+    return User.query.get(int(id))
